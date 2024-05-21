c71f9b3f8a51908d9cd62734f21fad14ee75116d
Faizan Mustafa <Faizanchaudhry671@gmail.com>
2024-05-22 01:33:26 +0500

    For Rating FrontEnd

--- lib/Screens/ViewFreeMovieSummary.dart
+++ lib/Screens/ViewFreeMovieSummary.dart
@@ -2,22 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:finalsemproject/API.dart';
 import 'package:finalsemproject/Screens/EditorAddCommentsFroWriterScreen.dart';
 import 'package:finalsemproject/Screens/WatchingScreen.dart';
-import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class ViewFreeMovieSummaryScreen extends StatefulWidget {
-  final int ?MovieID;
+  final int? MovieID;
+  final String? moviename;
 
-  final String?moviename;
-
-
-  const ViewFreeMovieSummaryScreen({Key? key ,
-    this.MovieID,
-
-    this.moviename
-  }) : super(key: key);
+  const ViewFreeMovieSummaryScreen({Key? key, this.MovieID, this.moviename}) : super(key: key);
 
   @override
   State<ViewFreeMovieSummaryScreen> createState() => _ViewFreeMovieSummaryScreenState();
@@ -28,19 +21,15 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
   String? writerName = '';
   Duration? endtime;
   Duration? startTime;
-  final Color Green  = Color(0xFF4FAA6D);
+  final Color Green = Color(0xFF4FAA6D);
   late final Map<String, dynamic> data;
-  //late List<dynamic> clipsData;
   List<Map<String, dynamic>> clipsData = [];
   List<Map<String, String>> clipsInfoList = [];
   List<Map<String, dynamic>> summariesData = [];
   late YoutubePlayerController controller;
   bool isPlayerReady = false;
-  // int index=0;
-  //bool assigned=false;
   int currentClipIndex = 0;
-  bool init=false;
-
+  bool init = false;
 
   Future<void> viewSentProject(int movieId) async {
     const String baseUrl = APIHandler.baseUrl1; // Update with your API base URL
@@ -59,7 +48,6 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
         List<dynamic> clipsListt = data['Clips'];
         clipsData = List<Map<String, dynamic>>.from(clipsListt);
 
-
         for (var clip in clipsData) {
           if (clip['isCompoundClip']) {
             clipsInfoList.add({
@@ -70,14 +58,11 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
           }
         }
 
-
-
         // Process the data as needed
-        print('Summaryies Data: $summariesData');
-        print('Clipssss Data: $clipsData');
-        print('ClipsInfo:$clipsInfoList');
+        print('Summaries Data: $summariesData');
+        print('Clips Data: $clipsData');
+        print('ClipsInfo: $clipsInfoList');
         refreshPage();
-
       } else {
         print('Failed to view sent project: ${response.reasonPhrase}');
       }
@@ -86,22 +71,18 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
     }
   }
 
-
-
   @override
   void initState() {
     super.initState();
-
     viewSentProject(widget.MovieID!);
     print('Title::${widget.moviename}');
   }
-  void refreshPage () {
+
+  void refreshPage() {
     setState(() {
       summaryText = summariesData.isNotEmpty ? summariesData.last['Summary1'].toString() : '';
-
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -253,119 +234,53 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Stack(children: [
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('Images/SplashScreen45.png'), // Your background image
-              fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('Images/SplashScreen45.png'), // Your background image
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.all(20),
-              //   child: Padding(
-              //     padding: const EdgeInsets.all(8.0),
-              //     child: Container(
-              //       width: 350,
-              //       height: 200,
-              //       decoration: BoxDecoration(
-              //           color: Colors.black,
-              //           borderRadius: BorderRadius.circular(10)),
-              //       child: Padding(
-              //         padding: const EdgeInsets.all(8.0),
-              //         child: Stack(
-              //           children: [
-              //             // Image
-              //             //
-              //             // isPlayerReady
-              //             //     ? YoutubePlayer(
-              //             //   controller: controller,
-              //             //   showVideoProgressIndicator: true,
-              //             //   onReady: () {
-              //             //     setState(() {
-              //             //       isPlayerReady = true;
-              //             //     });
-              //             //     print('Player is ready.');
-              //             //   },
-              //             // )
-              //             //     : loadingTextWidget,
-              //
-              //
-              //
-              //             // Text in the right bottom corner
-              //             // Column(
-              //             //   mainAxisAlignment: MainAxisAlignment.end,
-              //             //   crossAxisAlignment: CrossAxisAlignment.start,
-              //             //   children: [
-              //             //     Text(
-              //             //       'WAAR',
-              //             //       style: TextStyle(
-              //             //           color: Colors.white, // Set your desired text color
-              //             //           fontSize: 16,
-              //             //           fontWeight:
-              //             //           FontWeight.bold // Set your desired text size
-              //             //       ),
-              //             //     ),
-              //             //     Text(
-              //             //       'Bilal Lashari',
-              //             //       style: TextStyle(
-              //             //           color: Colors.yellow,
-              //             //           fontSize: 10,
-              //             //           fontWeight: FontWeight.bold),
-              //             //     ),
-              //             //     Text(
-              //             //       'Writer: Amman',
-              //             //       style: TextStyle(
-              //             //           color: Colors.yellow,
-              //             //           fontSize: 10,
-              //             //           fontWeight: FontWeight.bold),
-              //             //     ),
-              //             //   ],
-              //             // ),
-              //           ],
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 50,
-                width: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 4,
-                  ),
-                  gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [
-                      Colors.black,
-                      Colors.black,
-                      Colors.yellow,
-                      Colors.yellow
-                    ],
-                    stops: [0.0, 0.5, 0.5, 1.0],
-                  ),
-                  borderRadius: BorderRadius.circular(20),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 30,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: InkWell(
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 4,
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [
+                        Colors.black,
+                        Colors.black,
+                        Colors.yellow,
+                        Colors.yellow
+                      ],
+                      stops: [0.0, 0.5, 0.5, 1.0],
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
                           onTap: () {
                             // Navigator.push(
                             //     context,
@@ -377,120 +292,152 @@ class _ViewFreeMovieSummaryScreenState extends State<ViewFreeMovieSummaryScreen>
                             style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20,
-                                color: Colors.black,fontFamily: 'BigshotOne'),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: InkWell(
+                                color: Colors.black,
+                                fontFamily: 'BigshotOne'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
                           onTap: () {},
                           child: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              WatchingScreen(moviename: widget.moviename,clipsData: clipsData,ClipsInfoList: clipsInfoList,
-
-                                              )));
-                                });
-                              },
-                              child: Text(
-                                'Watch',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,fontFamily: 'BigshotOne'),
-                              ))),
-                    )
-                  ],
+                            onTap: () {
+                              setState(() {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => WatchingScreen(
+                                      moviename: widget.moviename,
+                                      clipsData: clipsData,
+                                      ClipsInfoList: clipsInfoList,
+                                    ),
+                                  ),
+                                );
+                              });
+                            },
+                            child: Text(
+                              'Watch',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontFamily: 'BigshotOne'),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: Container(
-                  height: 400,
-                  width: 300,
-                  decoration: BoxDecoration(
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Container(
+                    height: 400,
+                    width: 300,
+                    decoration: BoxDecoration(
                       color: Colors.black,
                       border: Border.all(
                         color: Colors.white,
                         width: 4,
                       ),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          summaryText ?? 'Loading summary...',
-                          style: TextStyle(fontSize: 20, color: Colors.white,fontFamily: 'BigshotOne',fontWeight: FontWeight.bold),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            summaryText ?? 'Loading summary...',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontFamily: 'BigshotOne',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-
-                      SizedBox(
-                        height: 10,
-                      ),
-                    ],
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Writer Rate:',
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontFamily: 'BigshotOne',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            StarRatingWidget(), // Add the star rating widget here
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Summary Rate:',
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontFamily: 'BigshotOne',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            StarRatingWidget(), // Add the star rating widget here
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
-      ],)
+        ],
+      ),
     );
   }
-//   void _onControllerStateChanged() {
-//     String startTimeStr = clipsData[currentClipIndex]['Start_time'].trim();
-//     String endTimeStr = clipsData[currentClipIndex]['End_time'].trim();
-//
-//     double startTimeDouble = double.parse(startTimeStr);
-//     double endTimeDouble = double.parse(endTimeStr);
-//
-// // Convert the double values to integers
-//     int startTimeInSeconds = startTimeDouble.toInt();
-//     int endTimeInSeconds = endTimeDouble.toInt();
-//
-// // Create Duration objects
-//     Duration startTime = Duration(seconds: startTimeInSeconds);
-//     Duration EndTime = Duration(seconds: endTimeInSeconds);
-//     final currentTime = controller.value.position;
-//     final  endTime = EndTime;
-//
-//
-//
-//
-//     if (currentTime >= endTime) {
-//       if(init)
-//         return;
-//       init=true;
-//
-//       // Current clip ended, load next clip
-//       if (currentClipIndex < clipsData.length - 1) {
-//         currentClipIndex++;
-//         controller.load(clipsData[currentClipIndex]['Url']);
-//         controller.seekTo(startTime);
-//         init=false;
-//       } else {
-//         currentClipIndex=0;
-//         controller.load(clipsData[currentClipIndex]['Url']);
-//         controller.seekTo(startTime);
-//         init=false;
-//         // All clips played
-//         // You can add logic here for what to do when all clips are played
-//       }
-//     }
-//   }
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     super.dispose();
-//   }
+}
+
+class StarRatingWidget extends StatefulWidget {
+  @override
+  _StarRatingWidgetState createState() => _StarRatingWidgetState();
+}
+
+class _StarRatingWidgetState extends State<StarRatingWidget> {
+  int _rating = 0;
+
+  void _setRating(int rating) {
+    setState(() {
+      _rating = rating;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return GestureDetector(
+          onTap: () => _setRating(index + 1),
+          child: Icon(
+            Icons.star,
+            color: index < _rating ? Colors.yellow : Colors.grey,
+            size: 30.0, // Adjust the size of the stars if needed
+          ),
+        );
+      }),
+    );
+  }
 }
